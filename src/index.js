17ae128aaed0b7c12aa19f8200972fa93469228f
Voy19 <denys.voytov@gmail.com>
2019-07-25 20:51:47 +0300

    '8/9'

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
+// function styled(data = null) {
+//    return function (options = '', ...args) {
+//       return function (props = {}) {
+//          let result = '';
+//          if (data !== null) {
+//             result = data(props);
+//          }
+//          result += options[0];
+//          if (args.length) {
+//             result += args.map((item, index) => item(props) + options[index + 1])
+//          }
+//          return result.replace(/\n|\t|\,/g, '');
+//       };
+//    };
+// }
+
 function styled(data = null) {
-   return function (options = '', ...args) {
-      return function (props = {}) {
-         let result = '';
-         if (data !== null) {
-            result = data(props);
+   if (!data) throw new Error("Wrong extended element");
+   return function (options = "", ...args) {
+      return function prop(props = {}) {
+         let result = "";
+         if (typeof data == "function") {
+            result += data(props);
          }
          result += options[0];
          if (args.length) {
-            result += args.map((item, index) => {
-               return item(props) + options[index + 1];
-            })
+            result += args.map((item, index) => test(item) + options[index + 1]);
          }
-         return result.replace(/\n|\t/g, '');
+
+         function test(a) {
+            return typeof a === 'function' ? test(a(props)) : a || '';
+         }
+
+         let arr = result.replace(/\n|\t|\,/g, "").replace(/;;/g, ";");
+         // return result.replace(/\n|\t|\,/g, "").replace(/;;/g, ";");
+         return arr;
+
       };
    };
 }
+
+styled.button = styled("button");
+styled.a = styled("a");
+styled.div = styled("div");
+styled.css = styled("css");
 
 module.exports = styled;
--- src/test.js
+++ src/test.js
@@ -1,71 +1,116 @@
-const styled = require('./index.js');
+const styled = require("./index.js");
+const css = styled.css;
 
-test('styled without params', () => {
-	expect(styled()
-		``()).toBe('');
+test("empty styled without params", () => {
+	expect(() => styled()
+		``()).toThrowError("Wrong extended element");
 });
 
-test('styled with simple css', () => {
-	expect(
-		styled()
-		`
-			color: red;
-		`()
-	).toBe('color: red;');
+test("styled div without params", () => {
+	expect(styled.div ``()).toBe("");
 });
 
-test('styled with multiple line css', () => {
+test("styled with simple css", () => {
+	expect(styled.button `color: red;`()).toBe("color: red;");
+});
+
+test("styled with multiple line css", () => {
 	expect(
-		styled()
-		`
+		styled.a `
 			color: white;
 			background: black;
 		`()
-	).toBe('color: white;background: black;');
+	).toBe("color: white;background: black;");
 });
 
-test('styled with query', () => {
-	const styledColor = styled()
-	`
-		color: ${argument => argument.color || 'red'};
+test("styled with query", () => {
+	const styledColor = styled.button `color: ${(argument) => argument.color || "red"};`;
+
+	expect(
+		styledColor({
+			color: "blue"
+		})
+	).toBe("color: blue;");
+
+	expect(styledColor()).toBe("color: red;");
+});
+
+test("styled with query multiline", () => {
+	const styledColor = styled.a `
+		color: ${(argument) => argument.color || "red"};
+		background: ${(argument) => argument.bg || "white"};
 	`;
 
 	expect(
 		styledColor({
-			color: 'blue'
+			color: "blue"
 		})
-	).toBe('color: blue;');
+	).toBe("color: blue;background: white;");
 
-	expect(
-		styledColor()
-	).toBe('color: red;');
+	expect(styledColor()).toBe("color: red;background: white;");
 });
 
-test('extended styled', () => {
-	const styledColor = styled()
-	`
-		color: ${argument => argument.color || 'red'};
-	`;
+test("extended styled", () => {
+	const styledColor = styled.a `color: ${(argument) => argument.color || "red"};`;
 
 	const styledColorWithBackground = styled(styledColor)
 	`
-		background: ${argument => argument.bg || 'white'};
+		background: ${(argument) => argument.bg || "white"};
 	`;
 
 	expect(
 		styledColorWithBackground({
-			color: 'blue'
+			color: "blue"
 		})
-	).toBe('color: blue;background: white;');
+	).toBe("color: blue;background: white;");
 
 	expect(
 		styledColorWithBackground({
-			color: 'yellow',
-			bg: 'blue'
+			color: "yellow",
+			bg: "blue"
 		})
-	).toBe('color: yellow;background: blue;');
+	).toBe("color: yellow;background: blue;");
+
+	expect(styledColorWithBackground()).toBe("color: red;background: white;");
+});
+
+test("mixin", () => {
+	const textAlign = css `text-align: ${(argument) => argument.align || "center"};`;
+
+	const font = css `font-family: Arial;`;
+
+	const styledText = styled.div `
+		${textAlign};
+		${font};
+	`;
 
 	expect(
-		styledColorWithBackground()
-	).toBe('color: red;background: white;');
+		styledText({
+			align: "left"
+		})
+	).toBe("text-align: left;font-family: Arial;");
+
+	expect(
+		styledText({
+			color: "yellow",
+			bg: "blue"
+		})
+	).toBe("text-align: center;font-family: Arial;");
+});
+
+test("extended styled without duplication", () => {
+	const styledColor = styled.a `color: black;`;
+
+	const extendedStyledColor = styled(styledColor)
+	`
+		${(argument) => argument.color && css`color: ${argument.color};`};
+	`;
+
+	expect(extendedStyledColor()).toBe("color: black;");
+
+	expect(
+		extendedStyledColor({
+			color: "blue"
+		})
+	).toBe("color: blue;");
 });
